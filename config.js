25a64943afdf530cbd239cd5d107a726f3913ed6
Oyasi Kelly <104142584+Oyasikelly@users.noreply.github.com>
2025-02-28 23:45:52 +0100

    handling submission using supabase

---
+++ config.js
@@ -0,0 +1,5 @@
+const CONFIG = {
+  SUPABASE_URL: "https://dzbyehukqpbnqokzpecc.supabase.co",
+  SUPABASE_ANON_KEY:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR6YnllaHVrcXBibnFva3pwZWNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA3NzY5NTgsImV4cCI6MjA1NjM1Mjk1OH0.eW6P35JDAvurm8gbfIlVNEBufiRs8onufk4owgY7Y1o",
+};
